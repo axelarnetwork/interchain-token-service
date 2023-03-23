7e0dfe0775365a62bfbcd4de79136b8f20479e7c
Foivos <foivos@umich.edu>
2023-03-23 17:36:49 +0200

    added the code.

--- contracts/interfaces/ITokenDeployer.sol
+++ contracts/interfaces/ITokenDeployer.sol
@@ -3,5 +3,11 @@
 pragma solidity 0.8.9;
 
 interface ITokenDeployer {
-    function deployToken(bytes calldata args, bytes32 salt) external payable returns (address tokenAddress);
+    function deployToken(
+        string calldata name,
+        string calldata symbol,
+        uint8 decimals,
+        address owner,
+        bytes32 salt
+    ) external payable returns (address tokenAddress);
 }
---
+++ contracts/utils/TokenDeployer.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+import { Create3Deployer } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/deploy/Create3Deployer.sol';
+
+import { ITokenDeployer } from '../interfaces/ITokenDeployer.sol';
+import { IERC20BurnableMintable } from '../interfaces/IERC20BurnableMintable.sol';
+
+contract TokenDeployer is ITokenDeployer {
+    Create3Deployer public immutable deployer;
+    address public immutable bytecodeServer;
+    address public immutable tokenImplementation;
+
+    constructor(address deployer_, address bytecodeServer_, address tokenImplementation_) {
+        deployer = Create3Deployer(deployer_);
+        bytecodeServer = bytecodeServer_;
+        tokenImplementation = tokenImplementation_;
+    }
+
+    function _getBytecode() internal view returns (bytes memory bytecode) {
+        bytecode = bytecodeServer.code;
+    }
+
+    function deployToken(
+        string calldata name,
+        string calldata symbol,
+        uint8 decimals,
+        address owner,
+        bytes32 salt
+    ) external payable returns (address tokenAddress) {
+        bytes memory bytecode = _getBytecode();
+        tokenAddress = deployer.deployAndInit(
+            bytecode,
+            salt,
+            abi.encodeWithSelector(IERC20BurnableMintable.setup.selector, abi.encode(name, symbol, decimals, owner))
+        );
+    }
+}
