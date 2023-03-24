67d343def04a8d258ebbef1e8848b7f570aaf6d4
Foivos <foivos@umich.edu>
2023-03-24 14:20:05 +0200

    added a deploy script and tests for the token deployer.

---
+++ contracts/utils/TokenDeployer.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.9;
+
+import { Create3Deployer } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/deploy/Create3Deployer.sol';
+
+import { ITokenDeployer } from '../interfaces/ITokenDeployer.sol';
+
+contract TokenDeployer is ITokenDeployer {
+    Create3Deployer public immutable deployer;
+    address public immutable bytecodeServer;
+    address public tokenImplementation;
+
+    constructor(address deployer_, address bytecodeServer_, address tokenImplementation_) {
+        deployer = Create3Deployer(deployer_);
+        bytecodeServer = bytecodeServer_;
+        tokenImplementation = tokenImplementation_;
+    }
+
+    function getBytecode(bytes calldata args) external view returns (bytes memory bytecode) {
+        uint256 bytecodeLen;
+        address server = bytecodeServer;
+        assembly {
+            bytecodeLen := extcodesize(server)
+        }
+        uint256 argsLen = args.length;
+        uint256 totalLen = argsLen + bytecodeLen;
+        bytecode = new bytes(totalLen);
+        uint256 start = bytecodeLen + 32;
+        assembly {
+            extcodecopy(server, add(bytecode, 32), 0, bytecodeLen)
+            calldatacopy(add(bytecode, start), args.offset, argsLen)
+            mstore(bytecode, totalLen)
+        }
+    }
+
+    function deployToken(        
+        string calldata name,
+        string calldata symbol,
+        uint8 decimals,
+        address owner,
+        bytes32 salt
+    ) external payable returns (address tokenAddress) {
+        bytes memory args = abi.encode(tokenImplementation, name, symbol, decimals, owner);
+        // convert args to calldata to handle more easily in the function
+        bytes memory bytecode = this.getBytecode(args);
+        tokenAddress = deployer.deploy(bytecode, salt);
+    }
+}
---
+++ scripts/deploy.js
@@ -0,0 +1,42 @@
+<<<<<<< Updated upstream
+=======
+'use strict';
+
+require('dotenv').config();
+
+const TokenDeployer = require('../artifacts/contracts/utils/TokenDeployer.sol/TokenDeployer.json');
+
+const BytecodeServer = require('../artifacts/contracts/utils/BytecodeServer.sol/BytecodeServer.json');
+const Token = require('../artifacts/contracts/utils/ERC20BurnableMintable.sol/ERC20BurnableMintable.json');
+const TokenProxy = require('../artifacts/contracts/proxies/TokenProxy.sol/TokenProxy.json');
+const { deployContract } = require('@axelar-network/axelar-gmp-sdk-solidity/scripts/utils');
+const { setJSON } = require('@axelar-network/axelar-local-dev');
+const { ContractFactory } = require('ethers');
+const chains = require(`../info/${process.env.ENV}.json`);
+
+async function deployTokenDeployer(chain, wallet) {
+    if (chain.tokenDeployer) return;
+
+    console.log(`Deploying ERC20BurnableMintable.`);
+    const token = await deployContract(wallet, Token, []);
+    chain.tokenImplementation = token.address;
+    console.log(`Deployed at: ${token.address}`);
+
+    console.log(`Deploying Bytecode Server.`);
+    const bytecodeServer = await deployContract(wallet, BytecodeServer, [TokenProxy.bytecode]);
+    chain.bytecodeServer = bytecodeServer.address;
+    console.log(`Deployed at: ${bytecodeServer.address}`);
+
+    console.log(`Deploying Token Deployer.`);
+    const tokenDeployer = await deployContract(wallet, TokenDeployer, [chain.create3Deployer, bytecodeServer.address, token.address]);
+    chain.tokenDeployer = tokenDeployer.address;
+    console.log(`Deployed at: ${tokenDeployer.address}`);
+
+    setJSON(chains, `./info/${process.env.ENV}.json`);
+    return tokenDeployer;
+}
+
+module.exports = {
+    deployTokenDeployer,
+};
+>>>>>>> Stashed changes
