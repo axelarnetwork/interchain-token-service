2f72af6e57690c61758471d45e14441682971403
Foivos <foivos@umich.edu>
2023-03-29 17:31:16 +0300

    Added support for deploying tokens through the service.

--- test/interchainTokenService.js
+++ test/interchainTokenService.js
@@ -1,23 +1,17 @@
 'use strict';
 
 const chai = require('chai');
-const {
-    getDefaultProvider,
-    Contract,
-    Wallet,
-} = require('ethers');
+const { getDefaultProvider, Contract, Wallet } = require('ethers');
 const { expect } = chai;
 const { keccak256, defaultAbiCoder } = require('ethers/lib/utils');
 require('dotenv').config();
 const Token = require('../artifacts/contracts/interfaces/IERC20BurnableMintable.sol/IERC20BurnableMintable.json');
-const Create3Deployer = require('@axelar-network/axelar-gmp-sdk-solidity/artifacts/contracts/deploy/Create3Deployer.sol/Create3Deployer.json');
 
 const { createAndExport } = require('@axelar-network/axelar-local-dev');
 
 let chain;
 let wallet;
-let otherWallet;
-let tokenDeployer;
+let tokenService;
 
 async function setupLocal(toFund) {
     await createAndExport({
@@ -25,70 +19,42 @@ async function setupLocal(toFund) {
         accountsToFund: toFund,
         relayInterval: 100,
         chains: ['Ethereum'],
+        port: 8502,
     });
     chain = require('../info/local.json')[0];
 }
 
 before(async () => {
     const deployerKey = keccak256(defaultAbiCoder.encode(['string'], [process.env.PRIVATE_KEY_GENERATOR]));
-    const otherKey = keccak256(defaultAbiCoder.encode(['string'], ['another key']));
     const deployerAddress = new Wallet(deployerKey).address;
-    const otherAddress = new Wallet(otherKey).address;
-    const toFund = [deployerAddress, otherAddress];
+    const toFund = [deployerAddress];
     await setupLocal(toFund);
     const provider = getDefaultProvider(chain.rpc);
     wallet = new Wallet(deployerKey, provider);
-    otherWallet = new Wallet(otherKey, provider);
-    const { deployTokenDeployer } = require('../scripts/deploy.js');
-
-    tokenDeployer = await deployTokenDeployer(chain, wallet);
+    const { deployTokenService, deployLinkerRouter, deployTokenDeployer } = require('../scripts/deploy.js');
+    await deployLinkerRouter(chain, wallet);
+    await deployTokenDeployer(chain, wallet);
+    tokenService = await deployTokenService(chain, wallet);
 });
 
-describe('Token', () => {
+describe('TokenService', () => {
     let token;
     const name = 'Test Token';
     const symbol = 'TT';
     const decimals = 13;
     const key = `asdasdasd`;
     const salt = keccak256(defaultAbiCoder.encode(['string'], [key]));
-    const amount = 12345;
 
-    before(async () => {
-        await tokenDeployer.deployToken(name, symbol, decimals, wallet.address, salt);
-        const deployer = new Contract(chain.create3Deployer, Create3Deployer.abi, wallet);
-        const tokenAddress = await deployer.deployedAddress(tokenDeployer.address, salt);
-        token = new Contract(tokenAddress, Token.abi, wallet);
-    });
+    before(async () => {});
 
-    it('Should Test that the token has the correct name, symbol, decimals and owner', async () => {
+    it('Should be able to deploy a native interchain token', async () => {
+        const deploymentAddress = await tokenService.getDeploymentAddress(wallet.address, salt);
+        // const tokenId = await tokenService.getOriginTokenId(deploymentAddress);
+        await tokenService.deployInterchainToken(name, symbol, decimals, wallet.address, salt, [], []);
+        token = new Contract(deploymentAddress, Token.abi, wallet);
         expect(await token.name()).to.equal(name);
         expect(await token.symbol()).to.equal(symbol);
         expect(await token.decimals()).to.equal(decimals);
-    });
-
-    it('Should be able to mint as the owner', async () => {
-        await token.mint(wallet.address, amount);
-
-        expect(Number(await token.balanceOf(wallet.address))).to.equal(amount);
-    });
-
-    it('Should not be able to mint as not the owner', async () => {
-        expect(token.connect(otherWallet).mint(wallet.address, amount)).to.be.revertedWith('NotOwner()');
-    });
-
-    it('Should not be able to burn as not the owner', async () => {
-        await token.approve(otherWallet.address, amount);
-        expect(token.connect(otherWallet).burnFrom(wallet.address, amount)).to.be.revertedWith('NotOwner()');
-    });
-
-    it('Should not be able to burn without approval', async () => {
-        // This reverts because of integer underflow so we do not expect a specific error;
-        expect(token.burnFrom(wallet.address, amount)).to.be.reverted;
-    });
-    it('Should be able to burn as the owner with approval', async () => {
-        await token.approve(wallet.address, amount);
-        await token.burnFrom(wallet.address, amount);
-
-        expect(Number(await token.balanceOf(wallet.address))).to.equal(0);
+        expect(await token.owner()).to.equal(wallet.address);
     });
 });
