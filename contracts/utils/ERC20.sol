e3aacd6ca823e4fdaf2447aed9ed52d1c15ddde9
Foivos <foivos@umich.edu>
2023-06-14 21:47:59 +0300

    feat: cleanup ERC20 token (#59)
    * removed a lot of files.
    
    * added interfaces and stub implementations
    
    * Changed from TokenDeployer to TokenManagerDeployer.
    
    * Added the TokenManagerProxy
    
    * Added the individual TokenManager implementations
    
    * stash
    
    * fixed implementations
    
    * fixed implementations
    
    * stash
    
    * added the _execute functionality
    
    * renamed
    
    * Token managers know their tokenId and its derivation is implemented
    
    * added functionality to deploy token managers
    
    * prettier
    
    * added a stub gateway that will do anything it is asked to
    
    * stash
    
    * implemented test logic
    
    * made lint happy
    
    * Implemented the requested name changes
    
    * fixed function visibility issue
    
    * first draft
    
    * finished with checking transmition
    
    * temp
    
    * some comments
    
    * fix duplicate code
    
    * finished tests
    
    * fixed some bugs
    
    * made lint happy
    
    * fixed tests
    
    * Added admins to token managers.
    
    * Some more fixes and implementations.
    
    * implemented flow limits
    
    * fixed tests and made lint happy
    
    * added express execution
    
    * implemented interchainToken functionality
    
    * prettier and lint
    
    * added ability to set flow limits from the service
    
    * Added pausing functionality
    
    * Added a lot of small fixes
    
    * made lint happy
    
    * Added permit functionality for the canonical token manager and did some cleanup of token stuff
    
    * prettier and lint
    
    * Added a liquidity pool token manager
    
    * Added the option to obtain the params from the smart contracts and also deploy custm token managers using proper inputs
    
    * added a getted for the chainName
    
    * getTokenAddress & optional execution
    
    * removed the BytecodeServer and delegating calls to the deployer now
    
    * Removed some testing
    
    * remove optional execution
    
    * made lint happy
    
    * removed ITokenManagerType from the deployer
    
    * added standardized tokens and the ability to deploy them
    
    * Removed the option to combine calls at a low level and added multicall
    
    * prettier and remove unsused function
    
    * fix tests
    
    * remove unsused function from ERC20
    
    ---------
    
    Co-authored-by: Milap Sheth <milap@axelar.network>

--- contracts/utils/ERC20.sol
+++ contracts/utils/ERC20.sol
@@ -141,8 +141,6 @@ contract ERC20 is IERC20 {
     function _transfer(address sender, address recipient, uint256 amount) internal virtual {
         if (sender == address(0) || recipient == address(0)) revert InvalidAccount();
 
-        _beforeTokenTransfer(sender, recipient, amount);
-
         balanceOf[sender] -= amount;
         balanceOf[recipient] += amount;
         emit Transfer(sender, recipient, amount);
@@ -159,8 +157,6 @@ contract ERC20 is IERC20 {
      */
     function _mint(address account, uint256 amount) internal virtual {
         if (account == address(0)) revert InvalidAccount();
-
-        _beforeTokenTransfer(address(0), account, amount);
 
         totalSupply += amount;
         balanceOf[account] += amount;
@@ -180,8 +176,6 @@ contract ERC20 is IERC20 {
      */
     function _burn(address account, uint256 amount) internal virtual {
         if (account == address(0)) revert InvalidAccount();
-
-        _beforeTokenTransfer(account, address(0), amount);
 
         balanceOf[account] -= amount;
         totalSupply -= amount;
@@ -207,22 +201,4 @@ contract ERC20 is IERC20 {
         allowance[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
-
-    // For the standardized token we provide, we can remove this method
-    /**
-     * @dev Hook that is called before any transfer of tokens. This includes
-     * minting and burning.
-     *
-     * Calling conditions:
-     *
-     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
-     * will be to transferred to `to`.
-     * - when `from` is zero, `amount` tokens will be minted for `to`.
-     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
-     * - `from` and `to` are never both zero.
-     *
-     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
-     */
-    // solhint-disable-next-line no-empty-blocks
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual {}
 }
