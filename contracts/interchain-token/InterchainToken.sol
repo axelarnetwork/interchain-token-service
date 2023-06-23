03e0fba43692519dd706c2599830831ebd0b7a7e
Foivos <foivos@umich.edu>
2023-06-23 19:05:13 +0300

    adding check for overflow

--- contracts/interchain-token/InterchainToken.sol
+++ contracts/interchain-token/InterchainToken.sol
@@ -63,6 +63,9 @@ abstract contract InterchainToken is IInterchainToken, ERC20 {
         if (tokenManagerRequiresApproval()) {
             uint256 allowance_ = allowance[sender][address(tokenManager)];
             if (allowance_ != type(uint256).max) {
+                if(allowance_ > type(uint256).max - amount) {
+                    allowance_ = type(uint256).max - amount;
+                }
                 _approve(sender, address(tokenManager), allowance_ + amount);
             }
         }
