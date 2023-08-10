d743395c171a9a3913c7888996f1eb2b4b1ca438
Dean <deanamiel1@gmail.com>
2023-08-10 11:37:30 -0400

    fix typos (#99)
    Co-authored-by: Dean Amiel <dean@axelar.network>

--- contracts/interfaces/IInterchainTokenExecutable.sol
+++ contracts/interfaces/IInterchainTokenExecutable.sol
@@ -13,7 +13,7 @@ interface IInterchainTokenExecutable {
      * @param sourceChain the name of the source chain
      * @param sourceAddress the address that sent the contract call
      * @param data the data to be proccessed
-     * @param tokenId the tokenId of the token manager managing the token. You can access it's address by querrying the service
+     * @param tokenId the tokenId of the token manager managing the token. You can access it's address by querying the service
      * @param amount the amount of token that was sent
      */
     function executeWithInterchainToken(
