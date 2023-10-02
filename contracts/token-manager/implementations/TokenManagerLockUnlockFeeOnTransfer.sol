5c06a3b678c72c3e57dc04da0f1f97cf47356946
Foivos <foivos@umich.edu>
2023-10-02 16:09:49 +0300

    remove duplictes from merge

--- contracts/token-manager/implementations/TokenManagerLockUnlockFeeOnTransfer.sol
+++ contracts/token-manager/implementations/TokenManagerLockUnlockFeeOnTransfer.sol
@@ -83,14 +83,4 @@ contract TokenManagerLockUnlockFee is TokenManagerAddressStorage, NoReEntrancy {
     function getParams(bytes memory operator, address tokenAddress) external pure returns (bytes memory params) {
         params = abi.encode(operator, tokenAddress);
     }
-
-    /**
-     * @notice Getter function for the parameters of a lock/unlock TokenManager. Mainly to be used by frontends.
-     * @param operator the operator of the TokenManager.
-     * @param tokenAddress the token to be managed.
-     * @return params the resulting params to be passed to custom TokenManager deployments.
-     */
-    function getParams(bytes memory operator, address tokenAddress) external pure returns (bytes memory params) {
-        params = abi.encode(operator, tokenAddress);
-    }
 }
