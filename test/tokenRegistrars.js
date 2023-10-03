09b66bb71c9ffa20eeed4bedb8b0c79b3206795e
Foivos <foivos@umich.edu>
2023-10-03 19:17:59 +0300

    lint and fix a merge bug

--- test/tokenRegistrars.js
+++ test/tokenRegistrars.js
@@ -16,10 +16,10 @@ const { deployAll, deployContract } = require('../scripts/deploy');
 // const SELECTOR_DEPLOY_TOKEN_MANAGER = 3;
 const SELECTOR_DEPLOY_AND_REGISTER_STANDARDIZED_TOKEN = 4;
 
-const LOCK_UNLOCK = 0;
-const MINT_BURN = 1;
+const LOCK_UNLOCK = 2;
+const MINT_BURN = 0;
 
-describe.only('Token Registrsrs', () => {
+describe('Token Registrsrs', () => {
     let wallet;
     let service, gateway, gasService, canonicalTokenRegistrar, standardizedTokenRegistrar;
     const name = 'tokenName';
@@ -86,25 +86,17 @@ describe.only('Token Registrsrs', () => {
     });
 
     describe('Standardized Token Registrar', async () => {
-        let token, tokenId;
-        const mintAmount = 1234;  
+        let tokenId;
+        const mintAmount = 1234;
 
-    
         it('Should register a token', async () => {
             const salt = keccak256('0x');
             tokenId = await standardizedTokenRegistrar.getStandardizedTokenId(wallet.address, salt);
             const tokenAddress = await standardizedTokenRegistrar.getStandardizedTokenAddress(wallet.address, salt);
             const params = defaultAbiCoder.encode(['bytes', 'address'], [standardizedTokenRegistrar.address, tokenAddress]);
             const tokenManager = new Contract(await service.getTokenManagerAddress(tokenId), ITokenManager.abi, wallet);
-            const token = new Contract(tokenAddress, IERC20.abi, wallet)
-            await expect(standardizedTokenRegistrar.deployStandardizedToken(
-                salt,
-                name,
-                symbol,
-                decimals,
-                mintAmount,
-                wallet.address,
-            ))
+            const token = new Contract(tokenAddress, IERC20.abi, wallet);
+            await expect(standardizedTokenRegistrar.deployStandardizedToken(salt, name, symbol, decimals, mintAmount, wallet.address))
                 .to.emit(service, 'StandardizedTokenDeployed')
                 .withArgs(tokenId, wallet.address, name, symbol, decimals, mintAmount, standardizedTokenRegistrar.address)
                 .and.to.emit(service, 'TokenManagerDeployed')
@@ -112,7 +104,7 @@ describe.only('Token Registrsrs', () => {
                 .and.to.emit(token, 'Transfer')
                 .withArgs(standardizedTokenRegistrar.address, wallet.address, mintAmount)
                 .and.to.emit(tokenManager, 'OperatorshipTransferred')
-                .withArgs(wallet.address)
+                .withArgs(wallet.address);
         });
 
         it('Should initiate a remote standardized token deployment with the same distributor', async () => {
@@ -123,15 +115,8 @@ describe.only('Token Registrsrs', () => {
             const tokenAddress = await standardizedTokenRegistrar.getStandardizedTokenAddress(wallet.address, salt);
             let params = defaultAbiCoder.encode(['bytes', 'address'], [standardizedTokenRegistrar.address, tokenAddress]);
             const tokenManager = new Contract(await service.getTokenManagerAddress(tokenId), ITokenManager.abi, wallet);
-            const token = new Contract(tokenAddress, IERC20.abi, wallet)
-            await expect(standardizedTokenRegistrar.deployStandardizedToken(
-                salt,
-                name,
-                symbol,
-                decimals,
-                mintAmount,
-                wallet.address,
-            ))
+            const token = new Contract(tokenAddress, IERC20.abi, wallet);
+            await expect(standardizedTokenRegistrar.deployStandardizedToken(salt, name, symbol, decimals, mintAmount, wallet.address))
                 .to.emit(service, 'StandardizedTokenDeployed')
                 .withArgs(tokenId, wallet.address, name, symbol, decimals, mintAmount, standardizedTokenRegistrar.address)
                 .and.to.emit(service, 'TokenManagerDeployed')
@@ -139,26 +124,44 @@ describe.only('Token Registrsrs', () => {
                 .and.to.emit(token, 'Transfer')
                 .withArgs(standardizedTokenRegistrar.address, wallet.address, mintAmount)
                 .and.to.emit(tokenManager, 'OperatorshipTransferred')
-                .withArgs(wallet.address)
+                .withArgs(wallet.address);
 
             params = defaultAbiCoder.encode(['bytes', 'address'], ['0x', token.address]);
             const payload = defaultAbiCoder.encode(
                 ['uint256', 'bytes32', 'string', 'string', 'uint8', 'bytes', 'bytes', 'uint256', 'bytes'],
-                [SELECTOR_DEPLOY_AND_REGISTER_STANDARDIZED_TOKEN, tokenId, name, symbol, decimals, wallet.address.toLowerCase(), '0x', 0, wallet.address.toLowerCase()],
+                [
+                    SELECTOR_DEPLOY_AND_REGISTER_STANDARDIZED_TOKEN,
+                    tokenId,
+                    name,
+                    symbol,
+                    decimals,
+                    wallet.address.toLowerCase(),
+                    '0x',
+                    0,
+                    wallet.address.toLowerCase(),
+                ],
             );
-
 
             await expect(
                 standardizedTokenRegistrar.deployRemoteStandarizedToken(salt, true, destinationChain, gasValue, { value: gasValue }),
             )
                 .to.emit(service, 'RemoteStandardizedTokenAndManagerDeploymentInitialized')
-                .withArgs(tokenId, name, symbol, decimals, wallet.address.toLowerCase(), '0x', 0, wallet.address.toLowerCase(), destinationChain, gasValue)
+                .withArgs(
+                    tokenId,
+                    name,
+                    symbol,
+                    decimals,
+                    wallet.address.toLowerCase(),
+                    '0x',
+                    0,
+                    wallet.address.toLowerCase(),
+                    destinationChain,
+                    gasValue,
+                )
                 .and.to.emit(gasService, 'NativeGasPaidForContractCall')
                 .withArgs(service.address, destinationChain, service.address.toLowerCase(), keccak256(payload), gasValue, wallet.address)
                 .and.to.emit(gateway, 'ContractCall')
                 .withArgs(service.address, destinationChain, service.address.toLowerCase(), keccak256(payload), payload);
-
-            
         });
 
         it('Should initiate a remote standardized token deployment without the same distributor', async () => {
@@ -169,15 +172,8 @@ describe.only('Token Registrsrs', () => {
             const tokenAddress = await standardizedTokenRegistrar.getStandardizedTokenAddress(wallet.address, salt);
             let params = defaultAbiCoder.encode(['bytes', 'address'], [standardizedTokenRegistrar.address, tokenAddress]);
             const tokenManager = new Contract(await service.getTokenManagerAddress(tokenId), ITokenManager.abi, wallet);
-            const token = new Contract(tokenAddress, IERC20.abi, wallet)
-            await expect(standardizedTokenRegistrar.deployStandardizedToken(
-                salt,
-                name,
-                symbol,
-                decimals,
-                mintAmount,
-                wallet.address,
-            ))
+            const token = new Contract(tokenAddress, IERC20.abi, wallet);
+            await expect(standardizedTokenRegistrar.deployStandardizedToken(salt, name, symbol, decimals, mintAmount, wallet.address))
                 .to.emit(service, 'StandardizedTokenDeployed')
                 .withArgs(tokenId, wallet.address, name, symbol, decimals, mintAmount, standardizedTokenRegistrar.address)
                 .and.to.emit(service, 'TokenManagerDeployed')
@@ -185,14 +181,23 @@ describe.only('Token Registrsrs', () => {
                 .and.to.emit(token, 'Transfer')
                 .withArgs(standardizedTokenRegistrar.address, wallet.address, mintAmount)
                 .and.to.emit(tokenManager, 'OperatorshipTransferred')
-                .withArgs(wallet.address)
+                .withArgs(wallet.address);
 
             params = defaultAbiCoder.encode(['bytes', 'address'], ['0x', token.address]);
             const payload = defaultAbiCoder.encode(
                 ['uint256', 'bytes32', 'string', 'string', 'uint8', 'bytes', 'bytes', 'uint256', 'bytes'],
-                [SELECTOR_DEPLOY_AND_REGISTER_STANDARDIZED_TOKEN, tokenId, name, symbol, decimals, '0x', '0x', 0, wallet.address.toLowerCase()],
+                [
+                    SELECTOR_DEPLOY_AND_REGISTER_STANDARDIZED_TOKEN,
+                    tokenId,
+                    name,
+                    symbol,
+                    decimals,
+                    '0x',
+                    '0x',
+                    0,
+                    wallet.address.toLowerCase(),
+                ],
             );
-
 
             await expect(
                 standardizedTokenRegistrar.deployRemoteStandarizedToken(salt, false, destinationChain, gasValue, { value: gasValue }),
@@ -203,8 +208,6 @@ describe.only('Token Registrsrs', () => {
                 .withArgs(service.address, destinationChain, service.address.toLowerCase(), keccak256(payload), gasValue, wallet.address)
                 .and.to.emit(gateway, 'ContractCall')
                 .withArgs(service.address, destinationChain, service.address.toLowerCase(), keccak256(payload), payload);
-
-            
         });
     });
 });
