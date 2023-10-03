67fb9471c3fdc35ee4519e7e12cff893cb016424
Foivos <foivos@umich.edu>
2023-10-03 16:55:18 +0300

    Fixed an import bug.

--- contracts/interchain-token-service/InterchainTokenService.sol
+++ contracts/interchain-token-service/InterchainTokenService.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.0;
 
 import { IAxelarGasService } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol';
+import { IAxelarGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGateway.sol';
 import { SafeTokenTransferFrom } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/utils/SafeTransfer.sol';
 import { IERC20 } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IERC20.sol';
 
