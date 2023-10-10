bedab794025ccc4017b38ade33a4056a2225eed3
Dean Amiel <dean@axelar.network>
2023-10-10 11:26:47 -0400

    fix: deployed bytecode

--- contracts/test/HardCodedConstantsTest.sol
+++ contracts/test/HardCodedConstantsTest.sol
@@ -10,6 +10,8 @@ import { Operatable } from '../utils/Operatable.sol';
 import { Pausable } from '../utils/Pausable.sol';
 
 contract TestTokenManager is TokenManagerLiquidityPool {
+    string public constant NAME = 'TestTokenManager';
+
     constructor(address interchainTokenService_) TokenManagerLiquidityPool(interchainTokenService_) {
         require(TOKEN_ADDRESS_SLOT == uint256(keccak256('token-address')) - 1, 'invalid constant');
         require(LIQUIDITY_POOL_SLOT == uint256(keccak256('liquidity-pool-slot')) - 1, 'invalid constant');
@@ -17,6 +19,8 @@ contract TestTokenManager is TokenManagerLiquidityPool {
 }
 
 contract TestDistributable is Distributable {
+    string public constant NAME = 'TestDistributable';
+
     constructor() {
         require(DISTRIBUTOR_SLOT == uint256(keccak256('distributor')) - 1, 'invalid constant');
         require(PROPOSED_DISTRIBUTOR_SLOT == uint256(keccak256('proposed-distributor')) - 1, 'invalid constant');
@@ -24,18 +28,24 @@ contract TestDistributable is Distributable {
 }
 
 contract TestFlowLimit is FlowLimit {
+    string public constant NAME = 'TestFlowLimit';
+
     constructor() {
         require(FLOW_LIMIT_SLOT == uint256(keccak256('flow-limit')) - 1, 'invalid constant');
     }
 }
 
 contract TestNoReEntrancy is NoReEntrancy {
+    string public constant NAME = 'TestNoReEntrancy';
+
     constructor() {
         require(ENTERED_SLOT == uint256(keccak256('entered')) - 1, 'invalid constant');
     }
 }
 
 contract TestOperatable is Operatable {
+    string public constant NAME = 'TestOperatable';
+
     constructor() {
         require(OPERATOR_SLOT == uint256(keccak256('operator')) - 1, 'invalid constant');
         require(PROPOSED_OPERATOR_SLOT == uint256(keccak256('proposed-operator')) - 1, 'invalid constant');
@@ -43,6 +53,8 @@ contract TestOperatable is Operatable {
 }
 
 contract TestPausable is Pausable {
+    string public constant NAME = 'TestPausable';
+
     constructor() {
         require(PAUSE_SLOT == uint256(keccak256('paused')) - 1, 'invalid constant');
     }
