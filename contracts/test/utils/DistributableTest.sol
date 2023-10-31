eca8befdc84d3d73b986f9d7ab4abb7e21d65b6a
Foivos <foivos@umich.edu>
2023-10-31 19:05:37 +0200

    we actually need those.

---
+++ contracts/test/utils/DistributableTest.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { Distributable } from '../../utils/Distributable.sol';
+
+contract DistributableTest is Distributable {
+    uint256 public nonce;
+
+    constructor(address distributor) {
+        _addDistributor(distributor);
+    }
+
+    function testDistributable() external onlyRole(uint8(Roles.DISTRIBUTOR)) {
+        nonce++;
+    }
+
+    function getDistributorRole() external pure returns (uint8) {
+        return uint8(Roles.DISTRIBUTOR);
+    }
+}
