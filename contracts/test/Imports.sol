40b3d6a5feff1e7a812b84d1d588f3241c8f3894
Kiryl Yermakou <rma4ok@gmail.com>
2023-11-02 19:14:40 -0400

    refactor(Mocks): using mocks from CGP and SDK

---
+++ contracts/test/Imports.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+// solhint-disable no-unused-import
+import { MockGateway } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/test/mocks/MockGateway.sol';
+import { AxelarGasService } from '@axelar-network/axelar-cgp-solidity/contracts/gas-service/AxelarGasService.sol';
