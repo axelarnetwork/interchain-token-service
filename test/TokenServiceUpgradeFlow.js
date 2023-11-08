926b079e96acafb4b3b99251469231dabbc78766
Dean <deanamiel1@gmail.com>
2023-11-07 20:24:44 -0500

    feat: token service upgrade test (#127)
    * feat: token service upgrade test
    
    * fix: tests
    
    * fix: tests
    
    * fix: tests
    
    * fix: tests
    
    * feat: call function on ITS post upgrade
    
    ---------
    
    Co-authored-by: Dean Amiel <dean@axelar.network>
    Co-authored-by: Milap Sheth <milap@axelar.network>

---
+++ test/TokenServiceUpgradeFlow.js
@@ -0,0 +1,234 @@
+'use strict';
+
+const chai = require('chai');
+const { expect } = chai;
+require('dotenv').config();
+const { ethers } = require('hardhat');
+const {
+    utils: { keccak256, Interface, defaultAbiCoder },
+    getContractAt,
+} = ethers;
+const { getCreate3Address } = require('@axelar-network/axelar-gmp-sdk-solidity');
+const { approveContractCall } = require('../scripts/utils');
+const { isHardhat, waitFor, getRandomBytes32, getPayloadAndProposalHash } = require('./utils');
+const {
+    deployContract,
+    deployMockGateway,
+    deployGasService,
+    deployInterchainTokenService,
+    deployTokenManagerImplementations,
+} = require('../scripts/deploy');
+const { getBytecodeHash } = require('@axelar-network/axelar-chains-config');
+const AxelarServiceGovernance = require('@axelar-network/axelar-gmp-sdk-solidity/artifacts/contracts/governance/AxelarServiceGovernance.sol/AxelarServiceGovernance.json');
+const Create3Deployer = require('@axelar-network/axelar-gmp-sdk-solidity/artifacts/contracts/deploy/Create3Deployer.sol/Create3Deployer.json');
+
+describe('Interchain Token Service Upgrade Flow', () => {
+    let wallet, otherWallet, signer1, signer2, signer3;
+    let service, gateway, gasService;
+    let tokenManagerDeployer, interchainTokenDeployer, tokenManagerImplementations;
+    let interchainTokenFactoryAddress;
+
+    let axelarServiceGovernanceFactory;
+    let axelarServiceGovernance;
+
+    let governanceAddress;
+    let buffer;
+
+    const governanceChain = 'Governance Chain';
+    const threshold = 2;
+    const deploymentKey = 'InterchainTokenService';
+    const chainName = 'Test';
+
+    const tokenName = 'Token Name';
+    const tokenSymbol = 'TN';
+    const tokenDecimals = 13;
+
+    async function testDeployTokenManager() {
+        const salt = getRandomBytes32();
+        const tokenId = await service.interchainTokenId(wallet.address, salt);
+        const tokenManager = await getContractAt('TokenManager', await service.tokenManagerAddress(tokenId), wallet);
+
+        const token = await deployContract(wallet, 'InterchainTokenTest', [tokenName, tokenSymbol, tokenDecimals, tokenManager.address]);
+        const params = defaultAbiCoder.encode(['bytes', 'address'], [wallet.address, token.address]);
+
+        await expect(service.deployTokenManager(salt, '', 0, params, 0))
+            .to.emit(service, 'TokenManagerDeployed')
+            .withArgs(tokenId, tokenManager.address, 0, params);
+    }
+
+    before(async () => {
+        [wallet, otherWallet, signer1, signer2, signer3] = await ethers.getSigners();
+        const signers = [signer1, signer2, signer3];
+        governanceAddress = otherWallet.address;
+
+        buffer = isHardhat ? 10 * 60 * 60 : 10;
+
+        const create3DeployerFactory = await ethers.getContractFactory(Create3Deployer.abi, Create3Deployer.bytecode, wallet);
+        const create3Deployer = await create3DeployerFactory.deploy().then((d) => d.deployed());
+        const interchainTokenServiceAddress = await getCreate3Address(create3Deployer.address, wallet, deploymentKey);
+        const interchainToken = await deployContract(wallet, 'InterchainToken');
+
+        gateway = await deployMockGateway(wallet);
+        gasService = await deployGasService(wallet);
+        tokenManagerDeployer = await deployContract(wallet, 'TokenManagerDeployer', []);
+        interchainTokenDeployer = await deployContract(wallet, 'InterchainTokenDeployer', [interchainToken.address]);
+        tokenManagerImplementations = await deployTokenManagerImplementations(wallet, interchainTokenServiceAddress);
+        interchainTokenFactoryAddress = await getCreate3Address(create3Deployer.address, wallet, deploymentKey + 'Factory');
+
+        axelarServiceGovernanceFactory = await ethers.getContractFactory(
+            AxelarServiceGovernance.abi,
+            AxelarServiceGovernance.bytecode,
+            wallet,
+        );
+
+        axelarServiceGovernance = await axelarServiceGovernanceFactory
+            .deploy(
+                gateway.address,
+                governanceChain,
+                governanceAddress,
+                buffer,
+                signers.map((signer) => signer.address),
+                threshold,
+            )
+            .then((d) => d.deployed());
+
+        service = await deployInterchainTokenService(
+            wallet,
+            create3Deployer.address,
+            tokenManagerDeployer.address,
+            interchainTokenDeployer.address,
+            gateway.address,
+            gasService.address,
+            interchainTokenFactoryAddress,
+            tokenManagerImplementations.map((impl) => impl.address),
+            chainName,
+            [],
+            deploymentKey,
+            axelarServiceGovernance.address,
+        );
+    });
+
+    it('should upgrade Interchain Token Service through AxelarServiceGovernance timeLock proposal', async () => {
+        const commandID = 0;
+        const target = service.address;
+        const nativeValue = 0;
+        const timeDelay = isHardhat ? 12 * 60 * 60 : 12;
+
+        const targetInterface = new Interface(service.interface.fragments);
+        const newServiceImplementation = await deployContract(wallet, 'InterchainTokenService', [
+            tokenManagerDeployer.address,
+            interchainTokenDeployer.address,
+            gateway.address,
+            gasService.address,
+            interchainTokenFactoryAddress,
+            chainName,
+            tokenManagerImplementations.map((impl) => impl.address),
+        ]);
+        const newServiceImplementationCodeHash = await getBytecodeHash(newServiceImplementation);
+        const setupParams = '0x';
+        const calldata = targetInterface.encodeFunctionData('upgrade', [
+            newServiceImplementation.address,
+            newServiceImplementationCodeHash,
+            setupParams,
+        ]);
+
+        const [payload, proposalHash, eta] = await getPayloadAndProposalHash(commandID, target, nativeValue, calldata, timeDelay);
+
+        const commandIdGateway = getRandomBytes32();
+        const sourceTxHash = keccak256('0x123abc123abc');
+        const sourceEventIndex = 17;
+
+        await approveContractCall(
+            gateway,
+            governanceChain,
+            governanceAddress,
+            axelarServiceGovernance.address,
+            payload,
+            sourceTxHash,
+            sourceEventIndex,
+            commandIdGateway,
+        );
+
+        const txExecute = await axelarServiceGovernance.execute(commandIdGateway, governanceChain, governanceAddress, payload);
+
+        const receiptExecute = await txExecute.wait();
+        const minimumEta = (await ethers.provider.getBlock(receiptExecute.blockNumber)).timestamp + buffer;
+        const finalEta = minimumEta > eta ? minimumEta : eta;
+
+        await expect(txExecute)
+            .to.emit(axelarServiceGovernance, 'ProposalScheduled')
+            .withArgs(proposalHash, target, calldata, nativeValue, finalEta);
+
+        await waitFor(timeDelay);
+
+        const tx = await axelarServiceGovernance.executeProposal(target, calldata, nativeValue);
+        const receipt = await tx.wait();
+        const executionTimestamp = (await ethers.provider.getBlock(receipt.blockNumber)).timestamp;
+
+        await expect(tx)
+            .to.emit(axelarServiceGovernance, 'ProposalExecuted')
+            .withArgs(proposalHash, target, calldata, nativeValue, executionTimestamp)
+            .and.to.emit(service, 'Upgraded')
+            .withArgs(newServiceImplementation.address);
+
+        await testDeployTokenManager();
+    });
+
+    it('should upgrade Interchain Token Service through AxelarServiceGovernance multisig proposal', async () => {
+        const commandID = 2;
+        const target = service.address;
+        const nativeValue = 0;
+
+        const targetInterface = new Interface(service.interface.fragments);
+        const newServiceImplementation = await deployContract(wallet, 'InterchainTokenService', [
+            tokenManagerDeployer.address,
+            interchainTokenDeployer.address,
+            gateway.address,
+            gasService.address,
+            interchainTokenFactoryAddress,
+            chainName,
+            tokenManagerImplementations.map((impl) => impl.address),
+        ]);
+        const newServiceImplementationCodeHash = await getBytecodeHash(newServiceImplementation);
+        const setupParams = '0x';
+        const calldata = targetInterface.encodeFunctionData('upgrade', [
+            newServiceImplementation.address,
+            newServiceImplementationCodeHash,
+            setupParams,
+        ]);
+
+        const [payload, proposalHash] = await getPayloadAndProposalHash(commandID, target, nativeValue, calldata);
+
+        const commandIdGateway = getRandomBytes32();
+        const sourceTxHash = keccak256('0x123abc123abc');
+        const sourceEventIndex = 17;
+
+        await approveContractCall(
+            gateway,
+            governanceChain,
+            governanceAddress,
+            axelarServiceGovernance.address,
+            payload,
+            sourceTxHash,
+            sourceEventIndex,
+            commandIdGateway,
+        );
+
+        await expect(axelarServiceGovernance.execute(commandIdGateway, governanceChain, governanceAddress, payload))
+            .to.emit(axelarServiceGovernance, 'MultisigApproved')
+            .withArgs(proposalHash, target, calldata, nativeValue);
+
+        await axelarServiceGovernance
+            .connect(signer1)
+            .executeMultisigProposal(target, calldata, nativeValue)
+            .then((tx) => tx.wait());
+
+        await expect(axelarServiceGovernance.connect(signer2).executeMultisigProposal(target, calldata, nativeValue))
+            .to.emit(axelarServiceGovernance, 'MultisigExecuted')
+            .withArgs(proposalHash, target, calldata, nativeValue)
+            .and.to.emit(service, 'Upgraded')
+            .withArgs(newServiceImplementation.address);
+
+        await testDeployTokenManager();
+    });
+});
--- test/utils.js
+++ test/utils.js
@@ -8,11 +8,11 @@ function getRandomBytes32() {
     return keccak256(defaultAbiCoder.encode(['uint256'], [Math.floor(new Date().getTime() * Math.random())]));
 }
 
+const isHardhat = network.name === 'hardhat';
+
 const getGasOptions = () => {
     return network.config.blockGasLimit ? { gasLimit: network.config.blockGasLimit.toString() } : { gasLimit: 5e6 }; // defaults to 5M gas for revert tests to work correctly
 };
-
-const isHardhat = network.name === 'hardhat';
 
 const expectRevert = async (txFunc, contract, error, args) => {
     if (network.config.skipRevertTests) {
@@ -28,6 +28,30 @@ const expectRevert = async (txFunc, contract, error, args) => {
     }
 };
 
+const getChainId = () => {
+    return network.config.chainId;
+};
+
+const getPayloadAndProposalHash = async (commandID, target, nativeValue, calldata, timeDelay) => {
+    let eta;
+
+    if (timeDelay) {
+        const block = await ethers.provider.getBlock('latest');
+        eta = block.timestamp + timeDelay - 12; // 12 second buffer for live network tests
+    } else {
+        eta = 0;
+    }
+
+    const proposalHash = keccak256(defaultAbiCoder.encode(['address', 'bytes', 'uint256'], [target, calldata, nativeValue]));
+
+    const payload = defaultAbiCoder.encode(
+        ['uint256', 'address', 'bytes', 'uint256', 'uint256'],
+        [commandID, target, calldata, nativeValue, eta],
+    );
+
+    return [payload, proposalHash, eta];
+};
+
 const waitFor = async (timeDelay) => {
     if (isHardhat) {
         await network.provider.send('evm_increaseTime', [timeDelay]);
@@ -37,15 +61,12 @@ const waitFor = async (timeDelay) => {
     }
 };
 
-const getChainId = () => {
-    return network.config.chainId;
-};
-
 module.exports = {
     getRandomBytes32,
+    isHardhat,
     getChainId,
     getGasOptions,
-    isHardhat,
     expectRevert,
+    getPayloadAndProposalHash,
     waitFor,
 };
