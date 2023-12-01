a16908d8f6c416bcb7ef1255de03fe1ab839de43
Foivos <foivos@umich.edu>
2023-12-01 07:45:56 +0200

    feat: added ability to approve the service instead of the token manager (#195)
    * Added ability to approve the service instead of the token maanger
    
    * prettier and slither
    
    * fixed slither
    
    * more slither fixing
    
    * Update contracts/InterchainTokenService.sol
    
    Co-authored-by: Milap Sheth <milap@axelar.network>
    
    * remove try, checking allowance instead
    
    * slither and lint
    
    * some renaming
    
    * fix: lint error
    
    * feat: add test
    
    ---------
    
    Co-authored-by: Milap Sheth <milap@axelar.network>
    Co-authored-by: Dean Amiel <dean@axelar.network>

--- contracts/token-manager/TokenManagerLockUnlockFee.sol
+++ contracts/token-manager/TokenManagerLockUnlockFee.sol
@@ -57,7 +57,12 @@ contract TokenManagerLockUnlockFee is TokenManager, ReentrancyGuard, ITokenManag
         IERC20 token = IERC20(this.tokenAddress());
         uint256 balanceBefore = token.balanceOf(address(this));
 
-        token.safeTransferFrom(from, address(this), amount);
+        if (token.allowance(from, address(this)) < amount) {
+            // slither-disable-next-line var-read-using-this
+            interchainTokenService.transferToTokenManager(this.interchainTokenId(), address(token), from, amount);
+        } else {
+            token.safeTransferFrom(from, address(this), amount);
+        }
 
         uint256 diff = token.balanceOf(address(this)) - balanceBefore;
         if (diff < amount) {
