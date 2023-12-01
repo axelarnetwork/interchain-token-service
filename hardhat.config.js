54c3463fa642819c239ad87178412b56418cba88
Milap Sheth <milap@axelar.network>
2023-12-01 05:46:09 -0500

    feat: add compiler overrides for Proxy contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,31 +10,41 @@ const chains = require(`@axelar-network/axelar-chains-config/info/${env}.json`);
 const keys = readJSON(`${__dirname}/keys.json`);
 const { networks, etherscan } = importNetworks(chains, keys);
 
+const optimizerSettings = {
+    enabled: true,
+    runs: 10000,
+    details: {
+        peephole: process.env.COVERAGE === undefined,
+        inliner: process.env.COVERAGE === undefined,
+        jumpdestRemover: true,
+        orderLiterals: true,
+        deduplicate: true,
+        cse: process.env.COVERAGE === undefined,
+        constantOptimizer: true,
+        yul: true,
+        yulDetails: {
+            stackAllocation: true,
+        },
+    },
+};
+const compilerSettings = {
+    version: '0.8.19',
+    settings: {
+        evmVersion: process.env.EVM_VERSION || 'london',
+        optimizer: optimizerSettings,
+    },
+};
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
     solidity: {
-        version: '0.8.19',
-        settings: {
-            evmVersion: process.env.EVM_VERSION || 'london',
-            optimizer: {
-                enabled: true,
-                runs: 10000,
-                details: {
-                    peephole: process.env.COVERAGE === undefined,
-                    inliner: process.env.COVERAGE === undefined,
-                    jumpdestRemover: true,
-                    orderLiterals: true,
-                    deduplicate: true,
-                    cse: process.env.COVERAGE === undefined,
-                    constantOptimizer: true,
-                    yul: true,
-                    yulDetails: {
-                        stackAllocation: true,
-                    },
-                },
-            },
+        compilers: [compilerSettings],
+        // Fix the Proxy bytecodes
+        overrides: {
+            "contracts/proxies/Proxy.sol": compilerSettings,
+            "contracts/proxies/TokenManagerProxy.sol": compilerSettings,
         },
     },
     defaultNetwork: 'hardhat',
