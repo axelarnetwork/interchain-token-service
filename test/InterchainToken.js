d670e7e5b0cd2a63bfbec858e846227e0859e74d
ahramy <ahram@interoplabs.io>
2024-10-17 10:49:58 -0700

    updated cgp solidity version to 6.4.0

--- test/InterchainToken.js
+++ test/InterchainToken.js
@@ -147,7 +147,7 @@ describe('InterchainToken', () => {
             const contractBytecodeHash = keccak256(contractBytecode);
 
             const expected = {
-                london: '0xa01cf28b0b6ce6dc3b466e995585d69486400d671fce0ea8d06beba583e6f3bb',
+                london: '0x75a10f371014f97a894e69395422a36cc9e9961e32371c0213ed514702966b24',
             }[getEVMVersion()];
 
             expect(contractBytecodeHash).to.be.equal(expected);
