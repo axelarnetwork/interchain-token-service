ab264ac1d989bff97490213e42b4b96763fb67ef
Foivos <foivos@umich.edu>
2024-12-20 18:23:14 +0200

    finaly fixed the tokenManager bytecode

--- contracts/interfaces/IBaseTokenManager.sol
+++ contracts/interfaces/IBaseTokenManager.sol
@@ -9,14 +9,12 @@ pragma solidity ^0.8.0;
 interface IBaseTokenManager {
     /**
      * @notice A function that returns the token id.
-     * @dev This is stored in the proxy and should not be called in the implementation, but it is included here so that the interface properly tells us what functions exist.
      */
     function interchainTokenId() external view returns (bytes32);
 
     /**
      * @notice A function that should return the address of the token.
      * Must be overridden in the inheriting contract.
-     * @dev This is stored in the proxy and should not be called in the implementation, but it is included here so that the interface properly tells us what functions exist.
      * @return address address of the token.
      */
     function tokenAddress() external view returns (address);
