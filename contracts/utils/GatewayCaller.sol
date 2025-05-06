6c905866c4c0923bf19f198064b47e8d46cd1bb5
ahramy <ahram@interoplabs.io>
2025-05-06 10:23:14 -0700

    Update contracts/utils/GatewayCaller.sol
    Co-authored-by: Milap Sheth <milap@interoplabs.io>

--- contracts/utils/GatewayCaller.sol
+++ contracts/utils/GatewayCaller.sol
@@ -38,8 +38,10 @@ contract GatewayCaller is IGatewayCaller {
         uint256 gasValue
     ) external payable override {
         if (gasValue > 0) {
-            uint256 executionGasLimit = 0;
+            // Gas for the ITS msg must be estimated off-chain
             bool estimateOnChain = false;
+            // No need to set the gas limit since it's not being estimated on-chain
+            uint256 executionGasLimit = 0;
             bytes memory params = '';
 
             // slither-disable-next-line arbitrary-send-eth
