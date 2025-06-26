a19668c4ae36f9ae056c2c8fb197f92ca44f39f3
isi8787 <38940784+isi8787@users.noreply.github.com>
2025-06-26 14:55:59 -0400

    Update contracts/interchain-token/HyperliquidDeployer.sol
    Co-authored-by: Milap Sheth <milap@interoplabs.io>

--- contracts/interchain-token/HyperliquidDeployer.sol
+++ contracts/interchain-token/HyperliquidDeployer.sol
@@ -5,12 +5,13 @@ pragma solidity ^0.8.0;
 import { IHyperliquidDeployer } from '../interfaces/IHyperliquidDeployer.sol';
 
 /**
- * @title HyperLiquidDeployer
- * @notice This contract explicitly reserves slot 0 for the deployer address.
- * This is specifically for the linking of ERC 20 tokens between Hyperliquid EVM and Core
+ * @title HyperliquidDeployer
+ * @notice This contract allows setting setting a deployer address associated to an Interchain token.
+ * It must be inherited first to ensure the deployer is stored in slot 0, as required by Hyperliquid Core.
+ * This is specifically for the linking of Interchain tokens between Hyperliquid EVM and Core
  * where the deployer address used to deploy a spot asset in HyperCore matches the address stored in the first storage slot
- * of the ERC 20 token deployed in Hyperliquid EVM via create2 mechanism.
- * Must be inherited first to ensure slot 0 is properly reserved.
+ * of the ERC20 token deployed in Hyperliquid EVM via create2 mechanism.
+ * ```
  */
 abstract contract HyperliquidDeployer is IHyperliquidDeployer {
     /// @dev Constant for slot 0 to avoid magic numbers
