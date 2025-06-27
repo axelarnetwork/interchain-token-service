c9b73a7035d7824d893dd730f82c4b5d7a8214be
isaac markus <isaac@interoplabs.io>
2025-06-27 13:03:19 -0400

    fix: change file and need to recommit

---
+++ contracts/test/TestHyperliquidInterchainToken.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import { HyperliquidInterchainToken } from '../interchain-token/HyperliquidInterchainToken.sol';
+
+contract TestHyperliquidInterchainToken is HyperliquidInterchainToken {
+    bool internal tokenManagerRequiresApproval_ = true;
+
+    error AllowanceExceeded();
+
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint8 decimals_,
+        address service_,
+        bytes32 tokenId_
+    ) HyperliquidInterchainToken(service_) {
+        name = name_;
+        symbol = symbol_;
+        decimals = decimals_;
+        tokenId = tokenId_;
+        _addMinter(msg.sender);
+    }
+
+    // You can add new helper/test functions here, but do not override parent functions.
+    function burnFrom(address account, uint256 amount) external {
+        uint256 currentAllowance = allowance[account][msg.sender];
+        if (currentAllowance < amount) revert AllowanceExceeded();
+        _approve(account, msg.sender, currentAllowance - amount);
+        _burn(account, amount);
+    }
+
+    function setTokenId(bytes32 tokenId_) external {
+        tokenId = tokenId_;
+    }
+}
