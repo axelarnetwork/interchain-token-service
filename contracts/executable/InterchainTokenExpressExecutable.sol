3b87a5cbaf4e471760a6ae387ac54ed904a77ce4
Chad0 <Sean329@users.noreply.github.com>
2025-12-03 14:44:35 -0800

    Remove redundant wording in contract documentation

--- contracts/executable/InterchainTokenExpressExecutable.sol
+++ contracts/executable/InterchainTokenExpressExecutable.sol
@@ -11,7 +11,7 @@ import { InterchainTokenExecutable } from './InterchainTokenExecutable.sol';
  * @dev This contract extends `InterchainTokenExecutable` to provide express execution capabilities. It is intended to be inherited by contracts
  * that implement express logic for interchain token actions. This contract will only be called by the interchain token service.
  *
- * IMPORTANT: Unlike regular execute, express execute can be called by anyone willing to pay for the tokens upfront.
+ * IMPORTANT: Express execute can be called by anyone willing to pay for the tokens upfront.
  * This means the parameters passed to express execute are NOT validated by the GMP gateway.
  * Only the token transfer itself should be considered to have value. The payload and any other metadata
  * should NOT be used for anything critical beyond manipulating the received tokens.
